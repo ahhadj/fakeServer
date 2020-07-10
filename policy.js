c4be4222b20a482808ce68c09248d60d870bc645
Deng,Jun <jdeng@microstrategy.com>
2020-07-10 11:05:41 +0800

    policy done

--- policy.js
+++ policy.js
@@ -21,7 +21,43 @@ const connectionTemplate = {
 
 const enumRuleType = [ 'equal', 'not equal', 'in', 'not in', 'include', 'contains' ];
 
-function fakeValue(example) {
+const resourceTemplate = {
+	category: 'string',
+	createTime: 0,
+	hostname: 'string',
+	id: 'string',
+	name: 'string',
+	realm: 'string',
+	riskScore: 0,
+	tags: []
+};
+
+const userTemplate = {
+	description: 'string',
+	email: 'string',
+	enabled: true,
+	firstName: 'string',
+	id: 'string',
+	lastName: 'string',
+	outerId: 'string',
+	phone: 'string',
+	realm: 'string',
+	username: 'string',
+	createTime: 0
+};
+
+function fakeValue(field, example) {
+	if (field.includes('Time')) {
+		return faker.date.between('2018-01-01', '2020-07-30').toDateString;
+	}
+	if (Array.isArray(example)) {
+		let arr = [];
+		let length = faker.random.number(3);
+		for (let i = 0; i < length; i++) {
+			arr.push(faker.random.word());
+		}
+		return arr;
+	}
 	switch (typeof example) {
 		case 'number':
 			return faker.random.number(100);
@@ -32,9 +68,9 @@ function fakeValue(example) {
 	}
 }
 
-function fakeConnectionRule() {
-	let field = faker.random.arrayElement(Object.keys(connectionTemplate));
-	let value = fakeValue(connectionTemplate[field]);
+function fakeRuleWithTemplate(template) {
+	let field = faker.random.arrayElement(Object.keys(template));
+	let value = fakeValue(field, template[field]);
 	return {
 		field,
 		type: faker.random.arrayElement(enumRuleType),
@@ -42,23 +78,37 @@ function fakeConnectionRule() {
 	};
 }
 
-function fakePolicy(id) {
-	let connectPolicy = {
+function fakePolicyWithTemplate(template) {
+	let policy = {
 		exclude: faker.random.boolean(),
 		rules: []
 	};
-	let connectPolicyNumber = faker.random.number(5);
-	for (let i = 0; i < connectPolicyNumber; i++) {
-		connectPolicy.rules.push(fakeConnectionRule());
+	let ruleNumber = faker.random.number(5);
+	for (let i = 0; i < ruleNumber; i++) {
+		policy.rules.push(fakeRuleWithTemplate(template));
 	}
+	return policy;
+}
+
+function fakePolicy(id) {
+	let connectPolicy = fakePolicyWithTemplate(connectionTemplate);
+	let resourcePolicy = fakePolicyWithTemplate(resourceTemplate);
+	let resourceStatusPolicy = fakePolicyWithTemplate(resourceTemplate);
+	let userPolicy = fakePolicyWithTemplate(userTemplate);
+	let userStatusPolicy = fakePolicyWithTemplate(userTemplate);
 	return {
 		exclude: faker.random.boolean(),
 		createTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
+		startTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
 		endTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
 		forbidden: faker.random.boolean(),
 		id,
 		name: faker.random.word(),
-		connectPolicy
+		connectPolicy,
+		resourcePolicy,
+		resourceStatusPolicy,
+		userPolicy,
+		userStatusPolicy
 	};
 }
 
