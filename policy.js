84f54364ff8b967fb5429bcec6d6170d8ad584ee
qwdengjun <fire@foxmail.com>
2020-07-13 20:30:06 +0800

    add attributes to policy

--- policy.js
+++ policy.js
@@ -1,126 +1,134 @@
-var faker = require('faker');
-var { fakeUser } = require('./common.js');
+var faker = require("faker");
+var { fakeUser } = require("./common.js");
+var { attributes } = require("./attributes.js");
 
 const connectionTemplate = {
-	createTime: 0,
-	description: 'string',
-	fileTransfer: true,
-	id: 'string',
-	maxConnections: 0,
-	maxConnectionsPerUser: 0,
-	name: 'string',
-	outerId: 'string',
-	password: 'string',
-	port: 0,
-	protocol: 'string',
-	realm: 'string',
-	riskScore: 0,
-	sensitivityLevel: 0,
-	sessionRecord: true,
-	username: 'string'
+  createTime: 0,
+  description: "string",
+  fileTransfer: true,
+  id: "string",
+  maxConnections: 0,
+  maxConnectionsPerUser: 0,
+  name: "string",
+  outerId: "string",
+  password: "string",
+  port: 0,
+  protocol: "string",
+  realm: "string",
+  riskScore: 0,
+  sensitivityLevel: 0,
+  sessionRecord: true,
+  username: "string",
 };
 
-const enumRuleType = [ 'equal', 'not equal', 'in', 'not in', 'include', 'contains' ];
+const enumRuleType = [
+  "equal",
+  "not equal",
+  "in",
+  "not in",
+  "include",
+  "contains",
+];
 
 const resourceTemplate = {
-	category: 'string',
-	createTime: 0,
-	hostname: 'string',
-	id: 'string',
-	name: 'string',
-	realm: 'string',
-	riskScore: 0,
-	tags: []
+  category: "string",
+  createTime: 0,
+  hostname: "string",
+  id: "string",
+  name: "string",
+  realm: "string",
+  riskScore: 0,
+  tags: [],
 };
 
 const userTemplate = {
-	description: 'string',
-	email: 'string',
-	enabled: true,
-	firstName: 'string',
-	id: 'string',
-	lastName: 'string',
-	outerId: 'string',
-	phone: 'string',
-	realm: 'string',
-	username: 'string',
-	createTime: 0
+  description: "string",
+  email: "string",
+  enabled: true,
+  firstName: "string",
+  id: "string",
+  lastName: "string",
+  outerId: "string",
+  phone: "string",
+  realm: "string",
+  username: "string",
+  createTime: 0,
 };
 
 function fakeValue(field, example) {
-	if (field.includes('Time')) {
-		return faker.date.between('2018-01-01', '2020-07-30').toDateString;
-	}
-	if (Array.isArray(example)) {
-		let arr = [];
-		let length = faker.random.number(3);
-		for (let i = 0; i < length; i++) {
-			arr.push(faker.random.word());
-		}
-		return arr;
-	}
-	switch (typeof example) {
-		case 'number':
-			return faker.random.number(100);
-		case 'string':
-			return faker.random.words(2);
-		case 'boolean':
-			return faker.random.boolean();
-	}
+  if (field.includes("Time")) {
+    return faker.date.between("2018-01-01", "2020-07-30").toDateString;
+  }
+  if (Array.isArray(example)) {
+    let arr = [];
+    let length = faker.random.number(3);
+    for (let i = 0; i < length; i++) {
+      arr.push(faker.random.word());
+    }
+    return arr;
+  }
+  switch (typeof example) {
+    case "number":
+      return faker.random.number(100);
+    case "string":
+      return faker.random.words(2);
+    case "boolean":
+      return faker.random.boolean();
+  }
 }
 
 function fakeRuleWithTemplate(template) {
-	let field = faker.random.arrayElement(Object.keys(template));
-	let value = fakeValue(field, template[field]);
-	return {
-		field,
-		type: faker.random.arrayElement(enumRuleType),
-		value
-	};
+  let field = faker.random.arrayElement(Object.keys(template));
+  let value = fakeValue(field, template[field]);
+  return {
+    field,
+    type: faker.random.arrayElement(enumRuleType),
+    value,
+  };
 }
 
 function fakePolicyWithTemplate(template) {
-	let policy = {
-		exclude: faker.random.boolean(),
-		rules: []
-	};
-	let ruleNumber = faker.random.number(5);
-	for (let i = 0; i < ruleNumber; i++) {
-		policy.rules.push(fakeRuleWithTemplate(template));
-	}
-	return policy;
+  let policy = {
+    exclude: faker.random.boolean(),
+    rules: [],
+  };
+  let ruleNumber = faker.random.number(5);
+  for (let i = 0; i < ruleNumber; i++) {
+    policy.rules.push(fakeRuleWithTemplate(template));
+  }
+  return policy;
 }
 
 function fakePolicy(id) {
-	let connectPolicy = fakePolicyWithTemplate(connectionTemplate);
-	let resourcePolicy = fakePolicyWithTemplate(resourceTemplate);
-	let resourceStatusPolicy = fakePolicyWithTemplate(resourceTemplate);
-	let userPolicy = fakePolicyWithTemplate(userTemplate);
-	let userStatusPolicy = fakePolicyWithTemplate(userTemplate);
-	return {
-		exclude: faker.random.boolean(),
-		createTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
-		startTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
-		endTime: faker.date.between('2018-01-01', '2020-07-30').toDateString,
-		forbidden: faker.random.boolean(),
-		id,
-		name: faker.random.word(),
-		connectPolicy,
-		resourcePolicy,
-		resourceStatusPolicy,
-		userPolicy,
-		userStatusPolicy
-	};
+  let connectPolicy = fakePolicyWithTemplate(connectionTemplate);
+  let resourcePolicy = fakePolicyWithTemplate(resourceTemplate);
+  let resourceStatusPolicy = fakePolicyWithTemplate(resourceTemplate);
+  let userPolicy = fakePolicyWithTemplate(userTemplate);
+  let userStatusPolicy = fakePolicyWithTemplate(userTemplate);
+  return {
+    exclude: faker.random.boolean(),
+    createTime: faker.date.between("2018-01-01", "2020-07-30").toDateString,
+    startTime: faker.date.between("2018-01-01", "2020-07-30").toDateString,
+    endTime: faker.date.between("2018-01-01", "2020-07-30").toDateString,
+    forbidden: faker.random.boolean(),
+    id,
+    name: faker.random.word(),
+    connectPolicy,
+    resourcePolicy,
+    resourceStatusPolicy,
+    userPolicy,
+    userStatusPolicy,
+  };
 }
 
 function generateData() {
-	let policies = [];
-	for (let id = 0; id < 80; id++) {
-		policies.push(fakePolicy(id));
-	}
-	let users = [ fakeUser(1, faker) ];
+  let policies = [];
+  for (let id = 0; id < 80; id++) {
+    policies.push(fakePolicy(id));
+  }
+  let users = [fakeUser(1, faker)];
 
-	return { policies, users };
+  return { policies, users, attributes };
 }
 
 module.exports = generateData;
