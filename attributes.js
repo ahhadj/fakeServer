0f447c00301c97b0fbe4289e3364038cf50f0336
qwdengjun <fire@foxmail.com>
2020-07-13 20:21:08 +0800

    refactor code

---
+++ attributes.js
@@ -0,0 +1,144 @@
+const attrType = {
+  name: "type",
+  type: "enum",
+  options: [
+    "Windows",
+    "Linux",
+    "Mac",
+    "Unix",
+    "WebApp",
+    "C/S App",
+    "pdf",
+    "image",
+    "word",
+    "excel",
+    "ppt",
+    "Folder",
+  ],
+  filterBy: "category",
+  filterRules: {
+    Desktop: ["Windows", "Linux", "Mac"],
+    Workstation: ["Windows", "Linux", "Mac"],
+    Server: ["Windows", "Linux", "Unix"],
+    WebApp: ["WebApp"],
+    "C/S App": ["C/S App"],
+    File: ["pdf", "image", "word", "excel", "ppt"],
+    Folder: ["Folder"],
+  },
+};
+
+const attrCategory = {
+  name: "category",
+  type: "enum",
+  options: [
+    "Desktop",
+    "Workstation",
+    "Server",
+    "WebApp",
+    "C/S App",
+    "File",
+    "Folder",
+  ],
+};
+
+const attrName = {
+  name: "name",
+  type: "string",
+  maxLength: 30,
+};
+
+const attrGroup = {
+  name: "group",
+  type: "enum",
+  options: [
+    "Security",
+    "Finance",
+    "Human Resource",
+    "R&D",
+    "IT",
+    "Sales",
+    "Marketing",
+  ],
+};
+const attrProtocol = {
+  name: "protocol",
+  type: "enum",
+  options: ["SSH", "HTTPS", "RDP"],
+};
+const attrTag = {
+  name: "tags",
+  type: "enum",
+  options: ["risk", "safe", "read only", "read write", "sensative"],
+};
+
+const attrDepartment = {
+  name: "department",
+  type: "enum",
+  options: ["TEC", "SALES", "IT", "Finance"],
+};
+
+const attrLocation = {
+  name: "location",
+  type: "string",
+  maxLength: 30,
+};
+
+const attrAddress = {
+  name: "address",
+  type: "string",
+  maxLength: 30,
+};
+
+const attrModel = {
+  name: "model",
+  type: "string",
+  maxLength: 30,
+};
+
+const attrRoute = {
+  id: "route",
+  type: "string",
+  maxLength: 30,
+};
+
+const resourceAttributes = [
+  attrType,
+  attrCategory,
+  attrGroup,
+  attrProtocol,
+  attrTag,
+  attrName,
+  attrDepartment,
+  attrLocation,
+  attrAddress,
+  attrModel,
+  attrRoute,
+];
+
+const attributes = [
+  {
+    id: "Resource",
+    name: "Resource",
+    fields: resourceAttributes,
+  },
+  {
+    id: "User",
+    name: "User",
+    fields: [],
+  },
+  {
+    id: "Connect",
+    name: "Connect",
+    fields: [],
+  },
+];
+
+module.exports = {
+  attributes,
+  attrType,
+  attrCategory,
+  attrDepartment,
+  attrGroup,
+  attrTag,
+  attrProtocol,
+};
--- resources.js
+++ resources.js
@@ -1,141 +1,14 @@
 var { fakeUser } = require("./common.js");
-var faker = require("faker");
-const { fake } = require("faker");
-
-const attrType = {
-  name: "type",
-  type: "enum",
-  options: [
-    "Windows",
-    "Linux",
-    "Mac",
-    "Unix",
-    "WebApp",
-    "C/S App",
-    "pdf",
-    "image",
-    "word",
-    "excel",
-    "ppt",
-    "Folder",
-  ],
-  filterBy: "category",
-  filterRules: {
-    Desktop: ["Windows", "Linux", "Mac"],
-    Workstation: ["Windows", "Linux", "Mac"],
-    Server: ["Windows", "Linux", "Unix"],
-    WebApp: ["WebApp"],
-    "C/S App": ["C/S App"],
-    File: ["pdf", "image", "word", "excel", "ppt"],
-    Folder: ["Folder"],
-  },
-};
-
-const attrCategory = {
-  name: "category",
-  type: "enum",
-  options: [
-    "Desktop",
-    "Workstation",
-    "Server",
-    "WebApp",
-    "C/S App",
-    "File",
-    "Folder",
-  ],
-};
-
-const attrName = {
-  name: "name",
-  type: "string",
-  maxLength: 30,
-};
-
-const attrGroup = {
-  name: "group",
-  type: "enum",
-  options: [
-    "Security",
-    "Finance",
-    "Human Resource",
-    "R&D",
-    "IT",
-    "Sales",
-    "Marketing",
-  ],
-};
-const attrProtocol = {
-  name: "protocol",
-  type: "enum",
-  options: ["SSH", "HTTPS", "RDP"],
-};
-const attrTag = {
-  name: "tags",
-  type: "enum",
-  options: ["risk", "safe", "read only", "read write", "sensative"],
-};
-
-const attrDepartment = {
-  name: "department",
-  type: "enum",
-  options: ["TEC", "SALES", "IT", "Finance"],
-};
-
-const attrLocation = {
-  name: "location",
-  type: "string",
-  maxLength: 30,
-};
-
-const attrAddress = {
-  name: "address",
-  type: "string",
-  maxLength: 30,
-};
-
-const attrModel = {
-  name: "model",
-  type: "string",
-  maxLength: 30,
-};
-
-const attrRoute = {
-  id: "route",
-  type: "string",
-  maxLength: 30,
-};
-
-const resourceAttributes = [
-  attrType,
+var {
+  attributes,
   attrCategory,
-  attrGroup,
-  attrProtocol,
-  attrTag,
-  attrName,
+  attrType,
   attrDepartment,
-  attrLocation,
-  attrAddress,
-  attrModel,
-  attrRoute,
-];
-
-const attributes = [
-  {
-    id: "Resource",
-    name: "Resource",
-    fields: resourceAttributes,
-  },
-  {
-    id: "User",
-    name: "User",
-    fields: [],
-  },
-  {
-    id: "Connect",
-    name: "Connect",
-    fields: [],
-  },
-];
+  attrGroup,
+  attrTag,
+  attrProtocol,
+} = require("./attributes.js");
+var faker = require("faker");
 
 const enumStatus = ["enabled", "disabled"];
 
@@ -175,7 +48,6 @@ function generateData() {
   for (let id = 0; id < 80; id++) {
     let c = faker.random.arrayElement(attrCategory.options);
     let t = faker.random.arrayElement(attrType.filterRules[c]);
-    console.log("category: " + c + ", type: " + t);
     let createTime = faker.date
       .between("2018-01-01", "2020-07-30")
       .toISOString()
